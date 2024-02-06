17c887a25849d17bec878af1871f0b7387576a5c
AdulIT <spilberg.max@bk.ru>
2024-02-06 11:08:27 +0600

    feat: learn more, text collapse accordion done

--- src/js/main.js
+++ src/js/main.js
@@ -1,103 +1,26 @@
 import '../scss/style.scss'
-
-// Logos slider
-const sliderContainer = document.querySelector('.clients__slider')
-const sliderCopy = sliderContainer.querySelector('.clients__slider-slide').cloneNode(true)
-sliderContainer.appendChild(sliderCopy)
+import ItcCollapse from '../js/collapse'
 
 // Navigation dropdown
-const dropdownIcon = document.querySelector('.menu__arrow-down')
+const dropdownIcon = document.querySelectorAll('.arrow-down')
 const dropdownMenu = document.querySelector('.dropdown')
 
-dropdownIcon.addEventListener('click', () =>
-{
-    dropdownMenu.classList.toggle('show')
+dropdownIcon.forEach(dropdown => {
+    dropdown.addEventListener('click', () =>
+    {
+        dropdownMenu.classList.toggle('show')
+    })
 })
 
-// Security section's slider
-const securitySlider = document.querySelector('.security-slider__wrapper')
-const securitySlides = document.querySelectorAll('.security-slider__slide')
-const securitySliderNavigationUp = document.querySelector('.slide-up')
-const securitySliderNavigationDown = document.querySelector('.slide-down')
+const elems = document.querySelectorAll('.collapse');
 
-const slidesLength = securitySlides.length
+const collapses = []
+elems.forEach((elem) => collapses.push(new ItcCollapse(elem)));
 
-let activeSlideIndex = 0
-let gap = 30
-
-// console.log(securitySlides[5].clientHeight);
-
-function changeSlide(direction)
+document.addEventListener('click', function(event)
 {
-    let height = securitySlides[activeSlideIndex].clientHeight;
-    // let nextHeight = securitySlides[activeSlideIndex + 1].clientHeight;
-    // securitySlider.style.maxHeight = `${height + nextHeight + 30}px`
-    // console.log(securitySlider.clientHeight);
-    console.log(height);
-    if (direction === 'down')
-    {
-        activeSlideIndex++
-        if (activeSlideIndex > slidesLength - 1)
-        {
-            activeSlideIndex = 0
-        }
-    } else if (direction === 'up')
-    {
-        activeSlideIndex--
-        if (activeSlideIndex < 0)
-        {
-            activeSlideIndex = slidesLength - 1
-        }
-    }
-    securitySlider.style.transform = `translateY(-${(height * activeSlideIndex)}px)`;
-}
-
-securitySliderNavigationUp.addEventListener('click', () => changeSlide('up'))
-securitySliderNavigationDown.addEventListener('click', () => changeSlide('down'))
-
-// Tabs
-
-const tabs = document.querySelectorAll('.tabs__btn'),
-      tabsContent = document.querySelectorAll('.employee__tabcontent'),
-      tabsParent = document.querySelector('.tabs');
-
-function hideTabContent()
-{
-    tabsContent.forEach(item =>
-    {
-        item.classList.add('hide')
-        item.classList.remove('show', 'fade')
-    })
-
-    tabs.forEach(item =>
-    {
-        item.classList.remove('tabs__btn-active')
-    })
-}
-
-function showTabContent(i = 0)
-{
-    tabsContent[i].classList.add('show', 'fade')
-    tabsContent[i].classList.remove('hide')
-    tabs[i].classList.add('tabs__btn-active')
-}
-
-hideTabContent()
-showTabContent()
-
-tabsParent.addEventListener('click', (event) =>
-{
-    const target = event.target
-
-    if (target && target.classList.contains('tabs__btn'))
-    {
-        tabs.forEach((item, i) =>
-        {
-            if (target == item)
-            {
-                hideTabContent()
-                showTabContent(i)
-            }
-        })
-    }
+    console.log(event.target.dataset.collapse);
+    if (event.target.dataset.collapse !== undefined) {
+        collapses.forEach(collapse => collapse.toggle())
+      }
 })
---
+++ src/js/slider.js
@@ -0,0 +1,90 @@
+// Logos slider
+
+const sliderContainer = document.querySelector('.clients__slider')
+const sliderCopy = sliderContainer.querySelector('.clients__slider-slide').cloneNode(true)
+sliderContainer.appendChild(sliderCopy)
+
+
+// Security section's slider
+const securitySlider = document.querySelector('.security-slider__wrapper')
+const securitySlides = document.querySelectorAll('.security-slider__slide')
+const securitySliderNavigationUp = document.querySelector('.slide-up')
+const securitySliderNavigationDown = document.querySelector('.slide-down')
+
+const slidesLength = securitySlides.length
+
+let activeSlideIndex = 0
+
+// console.log(securitySlides[5].clientHeight);
+
+function changeSlide(direction)
+{
+    let height = securitySlides[activeSlideIndex].clientHeight;
+
+    if (direction === 'down')
+    {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1)
+        {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'up')
+    {
+        activeSlideIndex--
+        if (activeSlideIndex < 0)
+        {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+    securitySlider.style.transform = `translateY(-${(height * activeSlideIndex)}px)`;
+}
+
+securitySliderNavigationUp.addEventListener('click', () => changeSlide('up'))
+securitySliderNavigationDown.addEventListener('click', () => changeSlide('down'))
+
+// Tabs
+
+const tabs = document.querySelectorAll('.tabs__btn'),
+      tabsContent = document.querySelectorAll('.employee__tabcontent'),
+      tabsParent = document.querySelector('.tabs');
+
+function hideTabContent()
+{
+    tabsContent.forEach(item =>
+    {
+        item.classList.add('hide')
+        item.classList.remove('show', 'fade')
+    })
+
+    tabs.forEach(item =>
+    {
+        item.classList.remove('tabs__btn-active')
+    })
+}
+
+function showTabContent(i = 0)
+{
+    tabsContent[i].classList.add('show', 'fade')
+    tabsContent[i].classList.remove('hide')
+    tabs[i].classList.add('tabs__btn-active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.addEventListener('click', (event) =>
+{
+    const target = event.target
+
+    if (target && target.classList.contains('tabs__btn'))
+    {
+        tabs.forEach((item, i) =>
+        {
+            if (target == item)
+            {
+                hideTabContent()
+                showTabContent(i)
+            }
+        })
+    }
+})
